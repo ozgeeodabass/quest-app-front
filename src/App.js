df93977022004b3884390a51d414d9b99c0f1520
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2024-03-16 02:29:00 +0300

    auth route added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import User from './components/User/User';
+import Auth from './components/Auth/Auth';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path ="/" element={<Home/>}></Route>
           <Route path ="/users/:userId" element={<User/>}></Route>
+          <Route path ="/auth" element={<Auth/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
